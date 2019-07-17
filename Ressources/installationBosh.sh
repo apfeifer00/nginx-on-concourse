0c16f87c0c403d78c86b34d13a9dfbf8bb305871
Alexander Pfeifer <apfeifer@evoila.de>
2019-07-17 12:38:34 +0200

    no message

--- Ressources/installationBosh.sh
+++ Ressources/installationBosh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update -y
-apt-get install -y wget git linux-headers-generic virtualbox virtualbox-qt virtualbox-dkms build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt1-dev libxml2-dev libssl-dev libreadline7 libreadline-dev libyaml-dev libsqlite3-dev sqlite3
+apt-get install -y virtualbox-dkms wget git linux-headers-generic virtualbox virtualbox-qt virtualbox-dkms build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt1-dev libxml2-dev libssl-dev libreadline7 libreadline-dev libyaml-dev libsqlite3-dev sqlite3
     
 wget https://github.com/cloudfoundry/bosh-cli/releases/download/v5.5.1/bosh-cli-5.5.1-linux-amd64
 chmod +x ./bosh-cli-5.5.1-linux-amd64
