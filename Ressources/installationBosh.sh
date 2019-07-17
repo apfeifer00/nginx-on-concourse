7172c8cdec2b313c917b2508da397c93e4bc27ec
Alexander Pfeifer <apfeifer@evoila.de>
2019-07-17 12:40:41 +0200

    no message

--- Ressources/installationBosh.sh
+++ Ressources/installationBosh.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
+sleep 100m
 apt-get update -y
-apt-get install -y virtualbox-dkms wget git linux-headers-generic virtualbox virtualbox-qt virtualbox-dkms build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt1-dev libxml2-dev libssl-dev libreadline7 libreadline-dev libyaml-dev libsqlite3-dev sqlite3
+apt-get install -y wget git linux-headers-generic
+apt-get install git -y
+apt-get install  -y
+apt-get install -y virtualbox virtualbox-qt virtualbox-dkms build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt1-dev libxml2-dev libssl-dev libreadline7 libreadline-dev libyaml-dev libsqlite3-dev sqlite3
     
 wget https://github.com/cloudfoundry/bosh-cli/releases/download/v5.5.1/bosh-cli-5.5.1-linux-amd64
 chmod +x ./bosh-cli-5.5.1-linux-amd64
 mv ./bosh-cli-5.5.1-linux-amd64 /usr/local/bin/bosh
-apt-get update -y
+bosh -v
     
-
-git clone https://github.com/concourse/concourse-bosh-deployment.git
-cd concourse-bosh-deployment/lite/
-bosh create-env concourse.yml \
-  -o ./infrastructures/virtualbox.yml \
-  -l ../versions.yml \
-  --vars-store vbox-creds.yml \
-  --state vbox-state.json \
-  -v internal_cidr=192.168.100.0/24 \
-  -v internal_gw=192.168.100.1 \
-  -v internal_ip=192.168.100.4 \
-  -v public_ip=192.168.100.4
-
-
-
-
-
+mkdir -p ~/bosh-env/virtualbox
+cd ~/bosh-env/virtualbox
+git clone https://github.com/cloudfoundry/bosh-deployment.git
+./bosh-deployment/virtualbox/create-env.sh
+bosh -e vbox env
+sleep 100m
 echo "Done........."
