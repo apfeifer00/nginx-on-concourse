349ad4a9bc362dc168cd0cb7b9db974a7d721c3e
Alexander Pfeifer <apfeifer@evoila.de>
2019-07-19 09:43:40 +0200

    no message

--- Ressources/installationNginx.sh
+++ Ressources/installationNginx.sh
@@ -3,7 +3,7 @@ apt-get install git -y
 
 bosh -e vbox envs
 
-git clone https://bitbucket.org/meshstack/ap-kt-cf/src/master/helloWorld/
+git clone ssh://master@bitbucket.org/meshstack/ap-kt-cf/src/master/helloWorld/
 
 
 
