1a2b8f63cc82eb7581cd2562eaddd8b40292d7d9
Alexander Pfeifer <apfeifer@evoila.de>
2019-07-19 09:33:26 +0200

    no message

--- Ressources/installationNginx.sh
+++ Ressources/installationNginx.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 apt-get install git -y
-#bosh upload-stemcell --sha1 d2ff5fd1c3e8b84ba17f6567fe29e4efb8d1a430 \
- # https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-xenial-go_agent?v=315.70
+bosh upload-stemcell --sha1 d2ff5fd1c3e8b84ba17f6567fe29e4efb8d1a430 https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-xenial-go_agent?v=315.70
 bosh -e vbox envs
 cd ~/workspace
-#git clone https://github.com/cloudfoundry-community/nginx-release.git
-#cd nginx-release
-#bosh -e ci ur https://github.com/cloudfoundry-community/nginx-release/releases/download/1.13.12/nginx-release-1.13.12.tgz
-#bosh -e ci -d nginx deploy manifests/nginx-lite.yml
+git clone https://github.com/cloudfoundry-community/nginx-release.git
+cd nginx-release
+bosh -e ci ur https://github.com/cloudfoundry-community/nginx-release/releases/download/1.13.12/nginx-release-1.13.12.tgz
+bosh -e ci -d nginx deploy manifests/nginx-lite.yml
 
 
 
