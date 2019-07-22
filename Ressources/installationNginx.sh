575cdf0271d743821d5c5b408ce341d83933fa40
Alexander Pfeifer <apfeifer@evoila.de>
2019-07-22 13:40:42 +0200

    no message

--- Ressources/installationNginx.sh
+++ Ressources/installationNginx.sh
@@ -4,13 +4,11 @@ apt-get install git -y
 #sleep 100m
 
 
-#bosh us https://s3.amazonaws.com/bosh-core-stemcells/warden/bosh-stemcell-3468-warden-boshlite-ubuntu-trusty-go_agent.tgz -n
 #git clone git@bitbucket.org:meshstack/ap-kt-cf.git
 cd ~/workspace
-git clone https://github.com/cloudfoundry-community/nginx-release.git
-cd nginx-release
-bosh ur https://github.com/cloudfoundry-community/nginx-release/releases/download/1.13.12/nginx-release-1.13.12.tgz -n
-bosh  -d nginx deploy manifests/nginx-lite.yml -n
+git clone https://github.com/apfeifer00/NginxBosh.git
+cd NginxBosh
+bosh -d nginx deploy ./manifests.yml -n
 
 
  
